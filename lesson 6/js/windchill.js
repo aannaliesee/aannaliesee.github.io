068dea266c0bb1d5070eccb3b1ab837488c8558c
aannaliesee <annaliese.kirkham@gmail.com>
2020-10-23 23:56:14 -0600

    responsive

---
+++ lesson 6/js/windchill.js
@@ -0,0 +1,24 @@
+function output(){
+    let tempF = parseFloat(document.getElementById("high").textContent);
+    let speed = parseFloat(document.getElementById("speed".textContent);
+
+    let output = windChill(tempF, speed);
+    document.getElementById("windChill").textContent = output.toFixed(2);
+
+}
+
+function windChill(tempF, speed) {
+    if (tempF <= 50 && speed >= 3) {
+        let t = tempF;
+        let s = soeed; 
+        let f = 35.74 + (0.6215 *t) - (35.75 * Math,pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16));
+
+        return f;
+    }
+    else {
+        let na="N/A";
+        return na;
+    }
+}
+
+output()
