18543f1c9daa59b7204ed01b08d8aa954f1f1a16
aannaliesee <annaliese.kirkham@gmail.com>
2020-10-24 10:55:25 -0600

    WindChill

--- lesson 6/js/windchill.js
+++ lesson 6/js/windchill.js
@@ -1,18 +1,19 @@
+
 function output(){
-    let tempF = parseFloat(document.getElementById("high").textContent);
-    let speed = parseFloat(document.getElementById("speed".textContent);
+    let tempF = parseFloat(document.getElementById("high").innerHTML);
+    let speed = parseFloat(document.getElementById("speed").innerHTML);
 
     let output = windChill(tempF, speed);
-    document.getElementById("windChill").textContent = output.toFixed(2);
+    windChill(tempF,speed);
+    document.getElementById("windChill").innerHTML = output;
 
 }
 
-function windChill(tempF, speed) {
+function windChill(tempF,speed) {
     if (tempF <= 50 && speed >= 3) {
         let t = tempF;
-        let s = soeed; 
-        let f = 35.74 + (0.6215 *t) - (35.75 * Math,pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16));
-
+        let s = speed; 
+        let f = 35.74 + (0.6215 *t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16));
         return f;
     }
     else {
@@ -20,5 +21,3 @@ function windChill(tempF, speed) {
         return na;
     }
 }
-
-output()
