345cf28ca4a91fce158e0020fa45614688dcf2e6
aannaliesee <annaliese.kirkham@gmail.com>
2020-11-28 21:00:40 -0700

    Update 3

--- lesson-11/js/weatherapi.js
+++ lesson-11/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=e997671470321879b0d29e832e8c1fb8';
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=e997671470321879b0d29e832e8c1fb8';
 
 fetch(apiURL)
   .then((response) => response.json())
